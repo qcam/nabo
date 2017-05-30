c20013b99a273579b71eccc00d119cc30d454ac4
Cẩm Huỳnh <huynhquancam@gmail.com>
2017-05-30 19:26:11 +0200

    Initial commit

---
+++ lib/nabo.ex
@@ -0,0 +1,18 @@
+defmodule Nabo do
+  @moduledoc """
+  Documentation for Nabo.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nabo.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nabo_test.exs
@@ -0,0 +1,8 @@
+defmodule NaboTest do
+  use ExUnit.Case
+  doctest Nabo
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
