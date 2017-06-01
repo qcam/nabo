8a60948e2bda5f6cccd191eb7ef9d9aaea0a1a94
Cẩm Huỳnh <huynhquancam@gmail.com>
2017-06-01 11:50:16 +0200

    Add test for Nabo.Metadata

--- lib/nabo/metadata.ex
+++ lib/nabo/metadata.ex
@@ -4,21 +4,25 @@ defmodule Nabo.Metadata do
   def from_string(meta_string) do
     case Poison.decode(meta_string) do
       {:ok, metadata} ->
-        title = Map.fetch!(metadata, "title")
-        slug = Map.fetch!(metadata, "slug")
-        date = Map.fetch!(metadata, "date")
-
-        {
-          :ok,
-          %__MODULE__{
-            title: title,
-            slug: slug,
-            date: date,
-            extras: metadata,
-          },
-        }
+        with {:ok, title} <- Map.fetch(metadata, "title"),
+             {:ok, slug} <- Map.fetch(metadata, "slug"),
+             {:ok, date} <- Map.fetch(metadata, "date"),
+             date <- Date.from_iso8601!(date) do
+          {
+            :ok,
+            %__MODULE__{
+              title: title,
+              slug: slug,
+              date: date,
+              extras: metadata,
+            },
+          }
+        else
+          :error ->
+            {:error, "Failed to parse metadata: Did you have title, slug, and date set?"}
+        end
       {:error, reason} ->
-        {:error, "Error when parse metadata: #{Exception.message(reason)}"}
+        {:error, "Got invalid json string #{meta_string}"}
     end
   end
 end
---
+++ test/nabo/metadata_test.exs
@@ -0,0 +1,26 @@
+defmodule Nabo.MetadataTest do
+  use ExUnit.Case, async: true
+
+  test "from_string/1 with meta string" do
+    string = ~s({"title":"Hello","slug":"hello","date":"2017-01-01"})
+    assert({:ok, metadata = %Nabo.Metadata{}} = Nabo.Metadata.from_string(string))
+    {:ok, date} = Date.new(2017, 1, 1)
+    expected = %Nabo.Metadata{
+      title: "Hello",
+      slug: "hello",
+      date: date,
+      extras: %{
+        "title" => "Hello",
+        "slug" => "hello",
+        "date" => "2017-01-01",
+      },
+    }
+    assert(expected == metadata)
+
+    string = ~s({"slug":"hello","date":"2017-01-01"})
+    assert({:error, _} = Nabo.Metadata.from_string(string))
+
+    string = ~s(invalid-json)
+    assert({:error, "Got invalid json string invalid-json"} = Nabo.Metadata.from_string(string))
+  end
+end
