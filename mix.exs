26cd5f32000120c879590f717e335e8dd3acb5e2
Cẩm Huỳnh <huynhquancam@gmail.com>
2017-06-01 08:47:44 +0200

    Add more detail to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,35 +1,27 @@
 defmodule Nabo.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [app: :nabo,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
+     name: "Nabo",
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [
       mod: {Nabo, []},
-      extra_applications: [:logger],
+      application: [:logger],
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:cowboy, "~> 1.1.2"},
@@ -39,5 +31,18 @@ defmodule Nabo.Mixfile do
       {:scrivener_list, "~> 1.0"},
       {:poison, "~> 3.1.0"},
     ]
+  end
+
+  defp package do
+    [maintainers: ["Cẩm Huỳnh"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/qcam/nabo"},
+     files: ~w(mix.exs README.md lib priv)]
+  end
+
+  defp description do
+    """
+    A simple blog engine for Elixir
+    """
   end
 end
