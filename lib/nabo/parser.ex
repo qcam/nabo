653bc4da035b85c765b7ae23cb755e8180b46a9b
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-05-15 21:53:43 +0200

    Fix typo in typespec

--- lib/nabo/parser.ex
+++ lib/nabo/parser.ex
@@ -87,5 +87,5 @@ defmodule Nabo.Parser do
   """
 
   @callback parse(data :: binary, options :: any) ::
-              {:ok, parsed :: any} :: {:error, reason :: any}
+              {:ok, parsed :: any} | {:error, reason :: any}
 end
