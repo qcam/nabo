d1276437c804ca186ef0ba55ef7feb793bf047da
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-05-18 14:51:27 +0200

    Ensure parsers are compiled (#13)

--- lib/nabo/compiler.ex
+++ lib/nabo/compiler.ex
@@ -1,48 +1,71 @@
 defmodule Nabo.Compiler do
   @moduledoc false
 
-  @default_pattern ~r/[\s\r\n]---[\s\r\n]/s
+  alias Nabo.Post
 
-  alias Nabo.{
-    Parser,
-    Post
-  }
+  defmodule Options do
+    @moduledoc false
+
+    defstruct [
+      metadata_parser: {Nabo.Parser.Front, []},
+      excerpt_parser: {Nabo.Parser.Markdown, []},
+      body_parser: {Nabo.Parser.Markdown, []},
+      split_pattern: ~r/[\s\r\n]---[\s\r\n]/s,
+      log_level: :warn
+    ]
+
+    def new(options) when is_list(options) do
+      options = struct!(__MODULE__, options)
+
+      ensure_parsers(options)
+
+      options
+    end
+
+    defp ensure_parsers(options) do
+      options
+      |> Map.take([:metadata_parser, :excerpt_parser, :body_parser])
+      |> Map.values()
+      |> Enum.each(&ensure_parser/1)
+    end
+
+    defp ensure_parser({module, _}) do
+      case Code.ensure_compiled(module) do
+        {:module, ^module} ->
+          if function_exported?(module, :parse, 2) do
+            module
+          else
+            raise ArgumentError, "Expect parser to be a Nabo.Parser"
+          end
+
+        {:error, reason} ->
+          raise ArgumentError, "Configured parser #{inspect(module)} is not available due to #{inspect(reason)}"
+      end
+    end
+  end
 
   def compile(data, options) do
-    {front_parser, front_parser_opts} =
-      Keyword.get(options, :front_parser, {Parser.Front, []})
+    %Options{
+      metadata_parser: metadata_parser,
+      excerpt_parser: excerpt_parser,
+      body_parser: body_parser,
+      split_pattern: split_pattern
+    } = options
 
-    {excerpt_parser, excerpt_parser_opts} =
-      Keyword.get(options, :excerpt_parser, {Parser.Markdown, []})
-
-    {body_parser, body_parser_opts} =
-      Keyword.get(options, :body_parser, {Parser.Markdown, []})
-
-    split_pattern =
-      Keyword.get(options, :split_pattern, @default_pattern)
-
-    assert_parsers_loaded!([front_parser, excerpt_parser, body_parser])
-
-    case split_parts(data, split_pattern) do
-      {:ok, {front, excerpt, body}} ->
-        with {:ok, metadata} <- front_parser.parse(front, front_parser_opts),
-             {:ok, parsed_excerpt} <- excerpt_parser.parse(excerpt, excerpt_parser_opts),
-             {:ok, parsed_body} <- body_parser.parse(body, body_parser_opts) do
-          post = Post.new(metadata, excerpt, parsed_excerpt, body, parsed_body)
-          {:ok, post}
-        else
-          {:error, reason} ->
-            {:error, reason}
-        end
-
+    with {:ok, {front, excerpt, body}} <- split_parts(data, split_pattern),
+         {:ok, metadata} <- parse(front, metadata_parser),
+         {:ok, parsed_excerpt} <- parse(excerpt, excerpt_parser),
+         {:ok, parsed_body} <- parse(body, body_parser) do
+      {:ok, Post.new(metadata, excerpt, parsed_excerpt, body, parsed_body)}
+    else
       {:error, reason} ->
         {:error, reason}
     end
   end
 
-  defp split_parts(data, pattern) do
+  defp split_parts(raw_post, pattern) do
     parts =
-      data
+      raw_post
       |> String.trim_leading()
       |> String.split(pattern, parts: 3)
 
@@ -53,16 +76,12 @@ defmodule Nabo.Compiler do
       [front, excerpt, body] ->
         {:ok, {front, excerpt, body}}
 
-      _other ->
-        {:error, "bad post format"}
+      _ ->
+        {:error, "cannot split post with the configured pattern #{inspect(pattern)}"}
     end
   end
 
-  defp assert_parsers_loaded!(parsers) do
-    Enum.each(parsers, fn parser ->
-      if not Code.ensure_loaded?(parser) do
-        raise ArgumentError, "Configured parser #{parser} is not available"
-      end
-    end)
+  defp parse(data, {parser, parser_options}) do
+    apply(parser, :parse, [data, parser_options])
   end
 end
